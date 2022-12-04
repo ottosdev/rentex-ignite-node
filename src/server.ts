4f4f680fcfc76b9724fafe389cca5e26414e8409
Otto Neto <ottoneto@MacBook-Pro-de-Otto.local>
2022-12-04 16:18:41 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import swaggerConf from './swagger.json';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerConf));
 
 app.use(router);
 
